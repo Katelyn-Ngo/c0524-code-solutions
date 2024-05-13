f0e9ecb2c8eee75d5d1b9b944035c709345e20a2
Katelyn Ngo <thienhuongngo13@gmail.com>
2024-05-13 21:30:05 +0000

    completed typescript-primitives-and-variables exercise.

--- typescript-primitives-and-variables/main.ts
+++ typescript-primitives-and-variables/main.ts
@@ -6,8 +6,8 @@ console.log(' Value of Full Name:', fullName);
 console.log('Value of isCool:', isCool);
 console.log('Value of totalPets:', totalPets);
 console.log('typeof fullName:', typeof fullName);
-console.log('type of isCool:', typeof true);
-console.log('typeof totalPets:', typeof 1);
+console.log('type of isCool:', typeof isCool);
+console.log('typeof totalPets:', typeof totalPets);
 
 const empty: null = null;
 console.log('value of empty:', empty);
@@ -15,4 +15,4 @@ console.log('typeof empty:', typeof empty);
 
 let nothing: undefined;
 console.log('Value of nothing:', nothing);
-console.log('type of nothing:', nothing);
+console.log('type of nothing:', typeof nothing);
