a56def64669299ac313f6cd9b2b9af0293ebe95f
Katelyn Ngo <thienhuongngo13@gmail.com>
2024-05-14 22:30:41 +0000

    Completed typescript-data-model exercise.

---
+++ typescript-data-model/order-history.js
@@ -0,0 +1,26 @@
+'use strict';
+const orderHistory = [
+  {
+    title: 'JavasScript for Impatient programmers',
+    date: 'august 4, 2020',
+    price: 34,
+  },
+  {
+    title: 'The Timeless Way of Building',
+    date: 'july 19, 2020',
+    price: 44.53,
+  },
+  {
+    title: 'Gamecube Controller Adapter',
+    date: 'july 7,2020',
+    price: 17.22,
+  },
+  {
+    title: 'GameCube Controller',
+    date: 'july 3, 2020',
+    price: 94.95,
+    title2: 'The Art of SQL',
+    date2: 'july 5, 2020',
+    price2: 33.99,
+  },
+];
