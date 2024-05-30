6eff5e0bd7a3e5a228193998e1b62168c64da6c4
Katelyn Ngo <thienhuongngo13@gmail.com>
2024-05-30 06:51:09 +0000

    finish typescript-light-bulb exercise.

---
+++ typescript-light-bulb/main.js
@@ -0,0 +1,17 @@
+'use strict';
+const $background = document.querySelector('.background');
+const $lightBulb = document.querySelector('.lightBulb');
+if (!$background) throw new Error('$background does not exist');
+if (!$lightBulb) throw new Error('$lightBulb does not exist');
+let lightOn = true;
+$lightBulb.addEventListener('click', () => {
+  if (lightOn) {
+    $lightBulb.className = 'lightBulb lb-night';
+    $background.className = 'background bg-night';
+    lightOn = false;
+  } else {
+    $lightBulb.className = 'lightBulb lb-day';
+    $background.className = 'background bg-day';
+    lightOn = true;
+  }
+});
