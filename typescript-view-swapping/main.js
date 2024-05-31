c1c41da955ed63d64bcde7bcb7a107a75c1055db
Katelyn Ngo <thienhuongngo13@gmail.com>
2024-05-31 22:47:23 +0000

    complete typescript-view-swapping exercise.

---
+++ typescript-view-swapping/main.js
@@ -0,0 +1,25 @@
+'use strict';
+const $tabContainer = document.querySelector('.tab-container');
+const $tab = document.querySelectorAll('.tab');
+const $view = document.querySelectorAll('.view');
+if (!$tabContainer) throw new Error('$tabContainer does not exist');
+$tabContainer.addEventListener('click', (event) => {
+  const $eventTarget = event.target;
+  if ($eventTarget.matches('.tab')) {
+    for (let i = 0; i < $tab.length; i++) {
+      if ($eventTarget === $tab[i]) {
+        $tab[i].className = 'active tab';
+      } else {
+        $tab[i].className = 'tab';
+      }
+    }
+  }
+  const $dataValue = $eventTarget.dataset.view;
+  for (let i = 0; i < $view.length; i++) {
+    if ($view[i].getAttribute('data-view') === $dataValue) {
+      $view[i].className = 'view ';
+    } else {
+      $view[i].className = 'view hidden';
+    }
+  }
+});
