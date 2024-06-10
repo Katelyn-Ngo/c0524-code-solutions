550f3fa1a930516001ed494267823d445812e09d
Katelyn Ngo <thienhuongngo13@gmail.com>
2024-06-10 17:15:31 +0000

    Rename branch, small change to commit

--- practice-interview-challenge/main.ts
+++ practice-interview-challenge/main.ts
@@ -10,10 +10,9 @@ function getRangeReport(start: number, end: number): rangeReport {
   if (typeof start !== 'number' || typeof end !== 'number' || start > end) {
     throw new Error('invalid input');
   }
-  let total = 0;
+  const total = 0;
   const odds: number[] = [];
   const evens: number[] = [];
   const range: number[] = [];
-  for (let i = start; i <= end; i++) {
-
-  }
+  for (let i = start; i <= end; i++) {}
+}
