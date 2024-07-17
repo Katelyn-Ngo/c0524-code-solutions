bfa0aa42955ba1e202ae42c991b72776b965695b
Katelyn Ngo <thienhuongngo13@gmail.com>
2024-07-17 19:24:08 +0000

    completed sql-join exercise.

---
+++ sql-join/what-and-when.sql
@@ -0,0 +1,12 @@
+select
+"f". "releaseYear",
+"g". "name" as "genreName"
+from
+"films" as "f"
+join
+"filmGenre" as "fg"
+using ("filmId")
+join
+"genres" as "g"
+using ("genreId")
+where "f". "title" = 'Boogie Amelie'
